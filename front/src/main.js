7a050f47910c62219f748d44fc411fd06a5c766c
raylicola <rope.champ.109@gmail.com>
2022-10-22 17:23:23 +0900

    feat: axiosの設定を追加

--- front/src/main.js
+++ front/src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import { Quasar } from 'quasar'
+import axios from 'axios'
 
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 import App from './App.vue'
 import router from "./routers"
+
+axios.defaults.baseURL = 'http://localhost:8080/wp-json/wp/v2/posts/'
+axios.defaults.withCredentials = true
 
 const myApp = createApp(App)
 
