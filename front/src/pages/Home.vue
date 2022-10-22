6cee0a0891f94aadda0d8290084789ad9f5af0e0
raylicola <rope.champ.109@gmail.com>
2022-10-22 17:59:33 +0900

    Quasarのテスト

--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -1,9 +1,36 @@
 <template>
   <h1>Home</h1>
+  <q-card style="max-width: 250px" v-for="post in posts">
+    <q-card-section>
+      <div class="text-h6">{{post.title.rendered}}</div>
+    </q-card-section>
+    <q-separator inset></q-separator>
+    <q-card-section>
+      <p><span v-html="post.content.rendered"></span></p>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
+import axios from 'axios'
+import { onMounted, ref } from 'vue';
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    const posts = ref('')
+    const getPosts = async () => {
+      const {data} = await axios.get('/')
+      posts.value = data
+    }
+
+    onMounted(() => {
+      getPosts()
+    })
+
+    return {
+      posts
+    }
+  }
 }
 </script>
